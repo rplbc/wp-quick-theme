3d2fbdc9dbeac2a8e31ef4916437afddae5cb851
rplbc <radoslawlb@gmail.com>
2023-02-25 13:04:10 +0100

    default page template

--- page.php
+++ page.php
@@ -5,8 +5,13 @@
  * @package Quick
  */
 
-get_header();
+get_header(); ?>
 
-the_content();
+<article class="inner page-article">
+    <div class="page-article-content">
+        <?php the_content(); ?>
+    </div>
+</article>
 
+<?php
 get_footer();
