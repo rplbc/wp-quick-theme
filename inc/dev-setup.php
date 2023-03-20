1b97efa9bd832ab698ac74401275560317623adf
rplbc <radoslawlb@gmail.com>
2023-03-21 00:14:42 +0100

    remove admin side scripts

--- inc/dev-setup.php
+++ inc/dev-setup.php
@@ -6,14 +6,6 @@
  */
 
 add_action(
-    'admin_enqueue_scripts',
-    function () {
-        wp_enqueue_script( 'editor-styles', 'http://localhost:3003/js/editor-styles.js', [], null, true );
-        remove_editor_styles();
-    }
-);
-
-add_action(
     'wp_enqueue_scripts',
     function () {
         wp_dequeue_script( 'scripts' );
