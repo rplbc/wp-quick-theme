b60f6658bb672fc1dcdfa52c6ae6290a1a22657b
rplbc <radoslawlb@gmail.com>
2023-03-21 02:18:26 +0100

    improve dev setup

--- inc/dev-setup.php
+++ inc/dev-setup.php
@@ -5,25 +5,50 @@
  * @package Quick
  */
 
+namespace Quick\Dev;
+
+add_action(
+    'after_setup_theme',
+    function () {
+        add_editor_style( 'http://localhost:3003/css/editor-styles.css' );
+    }
+);
+
 add_action(
     'wp_enqueue_scripts',
     function () {
-        wp_dequeue_script( 'scripts' );
-        wp_dequeue_style( 'styles' );
+        wp_dequeue_script( 'quick-scripts' );
+        wp_dequeue_style( 'quick-styles' );
 
         // Keep the order or webpack fails.
-        wp_enqueue_script( 'scripts-dev', 'http://localhost:3003/js/main.js', [], null, true );
         wp_enqueue_script( 'dev', 'http://localhost:3003/js/dev.js', [], null, true );
+        wp_enqueue_script( 'scripts-dev', 'http://localhost:3003/js/main.js', [], null, true );
     }
 );
 
+/**
+ * Add modal with ACF fields
+ */
 add_action(
     'wp_footer',
-    function() {
-        echo '
-<div class="acf-preview">
-    <pre>' . esc_html( wp_json_encode( get_fields(), JSON_PRETTY_PRINT ) ) . '</pre>
-    <pre>' . esc_html( wp_json_encode( get_fields( 'quick_options' ), JSON_PRETTY_PRINT ) ) . '</pre>
-</div>';
+    function() { ?>
+
+    <div class="acf-preview">
+        <div class="acf-preview-content">
+            <?php quick_json( get_fields() ); ?>
+            <?php quick_json( get_fields( 'quick_options' ) ); ?>
+        </div>
+    </div>
+
+        <?php
     }
 );
+
+/**
+ * Outputs json encoded data.
+ *
+ * @param mixed $data Data to encode.
+ */
+function quick_json( mixed $data ) {
+    echo '<pre>' . esc_html( wp_json_encode( $data, JSON_PRETTY_PRINT ) ) . '</pre>';
+}
