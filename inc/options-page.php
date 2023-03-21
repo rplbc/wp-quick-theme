9b332818bb2a5e345080b6da2ebae64f448dabb2
rplbc <radoslawlb@gmail.com>
2023-03-21 02:12:50 +0100

    add missing textdomain

--- inc/options-page.php
+++ inc/options-page.php
@@ -8,8 +8,8 @@
 if ( function_exists( 'acf_add_options_page' ) ) {
     acf_add_options_page(
         [
-            'page_title' => __( 'Theme options' ),
-            'menu_title' => __( 'Quick' ),
+            'page_title' => __( 'Theme options', 'quick' ),
+            'menu_title' => __( 'Quick', 'quick' ),
             'menu_slug'  => 'quick-options',
             'icon_url'   => 'dashicons-admin-home',
             'position'   => 1,
