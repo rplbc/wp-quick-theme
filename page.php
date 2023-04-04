139711ac1529999d05f3870237c4ed0ab66a906e
rplbc <radoslawlb@gmail.com>
2023-04-04 21:23:46 +0200

    add docs sample

--- page.php
+++ page.php
@@ -9,8 +9,6 @@ get_header(); ?>
 
 <article class="inner page-article">
     <div class="page-article-content">
-        <?php get_template_part( 'parts/socials/follow' ); ?>
-        <?php get_template_part( 'parts/socials/share' ); ?>
         <?php the_content(); ?>
     </div>
 </article>
