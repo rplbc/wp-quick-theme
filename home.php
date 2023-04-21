f87bba313c3ab9daca900e0a9b6babe2aaa0e34f
rplbc <radoslawlb@gmail.com>
2023-04-21 11:48:41 +0200

    refactor: use section tag as blog wrapper

--- home.php
+++ home.php
@@ -10,24 +10,24 @@
 get_header();
 ?>
 
-<div class="inner bp-inner">
-<div class="pre-heading">Blog</div>
-<h1 class="h1-title">Latest posts</h1>
-<?php
-if ( have_posts() ) :
-    ?>
-    <div class="grid bp-grid">
+<section class="inner bp-inner">
+    <div class="pre-heading">Blog</div>
+    <h1 class="h1-title">Latest posts</h1>
     <?php
-    while ( have_posts() ) :
-        the_post();
-        get_template_part( 'parts/blog/excerpt' );
-    endwhile;
+    if ( have_posts() ) :
+        ?>
+        <div class="grid bp-grid">
+        <?php
+        while ( have_posts() ) :
+            the_post();
+            get_template_part( 'parts/blog/excerpt' );
+        endwhile;
+        ?>
+        </div>
+        <?php
+    endif;
     ?>
-    </div>
-    <?php
-endif;
-?>
-</div>
+</section>
 
 <?php
 get_footer();
