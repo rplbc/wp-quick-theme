b98529919cc6a58ab64e222bd704a05e9172fbc5
rplbc <radoslawlb@gmail.com>
2023-06-25 14:36:48 +0200

    chore: init tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './inc/**/*.php',
+    './parts/**/*.php',
+    './src/ts/**/*.ts',
+    './templates/**/*.php',
+    './*.php',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
