4e36eb6f077a3602b6bd5b0dbb87c253930b7ca6
rplbc <radoslawlb@gmail.com>
2023-07-13 13:17:49 +0200

    refactor: put webpack config into a single file

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,46 +1,130 @@
+import ESLintPlugin from 'eslint-webpack-plugin'
+import FileManagerPlugin from 'filemanager-webpack-plugin'
+import MiniCssExtractPlugin, {
+  loader as miniCSSLoader,
+} from 'mini-css-extract-plugin'
 import path from 'path'
 import type { Configuration } from 'webpack'
 import 'webpack-dev-server'
-import * as cfg from './webpack'
+import RemoveEmptyScriptsPlugin from 'webpack-remove-empty-scripts'
 
-const BUILD_DIR = 'dist'
-const EXPORT_DIR = '.quick'
-const DEVSERVER_PORT = 3003
+const exportDir = '.quick'
+const distDir = 'dist'
+const devServerPort = 3003
+const dirsToExport = [distDir, 'acf-json', 'inc', 'parts', 'templates']
 
-const webpackConfig: WebpackConfig = (env) => {
-  const { WEBPACK_WATCH, WEBPACK_SERVE, WEBPACK_BUILD } = env || {}
+export default function (env = { WEBPACK_SERVE: false }): Configuration {
+  const { WEBPACK_SERVE: isDev = false } = env
 
   return {
-    entry: cfg.entry(WEBPACK_SERVE),
+    entry: {
+      main: './src/ts/main.ts',
+      ...(isDev
+        ? {
+            dev: './src/ts/dev.ts',
+          }
+        : {
+            styles: './src/scss/main.scss',
+            'editor-styles': './src/scss/editor.scss',
+          }),
+    },
     output: {
       filename: 'js/[name].js',
-      path: path.join(__dirname, BUILD_DIR),
+      path: path.resolve(__dirname, distDir),
       clean: true,
     },
     resolve: {
       extensions: ['.ts', '...'],
     },
-    mode: WEBPACK_SERVE || WEBPACK_WATCH ? 'development' : 'production',
-    devtool: WEBPACK_SERVE || WEBPACK_WATCH ? false : 'source-map',
-    devServer: cfg.devServer(DEVSERVER_PORT),
-    module: cfg.modules(WEBPACK_SERVE),
-    plugins: cfg.plugins(
-      WEBPACK_SERVE,
-      WEBPACK_BUILD ? path.join(__dirname, EXPORT_DIR) : undefined
-    ),
+    mode: isDev ? 'development' : 'production',
+    devtool: 'source-map',
+    devServer: {
+      port: devServerPort,
+      hot: true,
+      watchFiles: [
+        'inc/**/*.php',
+        'parts/**/*.php',
+        'templates/**/*.php',
+        '*.php',
+      ],
+      allowedHosts: ['localhost', '.local'],
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'swc-loader',
+            options: {
+              jsc: {
+                parser: {
+                  syntax: 'typescript',
+                  tsx: false,
+                },
+              },
+            },
+          },
+        },
+        {
+          test: /\.scss$/,
+          exclude: /node_modules/,
+          use: [
+            isDev ? 'style-loader' : miniCSSLoader,
+            'css-loader',
+            'postcss-loader',
+            'sass-loader',
+          ],
+        },
+        {
+          test: /\.(woff|woff2)$/,
+          type: 'asset',
+          generator: {
+            filename: './fonts/[name][ext]',
+          },
+        },
+      ],
+    },
+    plugins: [
+      ...(!isDev
+        ? [
+            new FileManagerPlugin({
+              events: {
+                onStart: {
+                  delete: [exportDir],
+                },
+                onEnd: {
+                  copy: [
+                    {
+                      source: '*.{php,css}|theme.json',
+                      destination: exportDir,
+                    },
+                    ...dirsToExport.map((name) => ({
+                      source: name,
+                      destination: `${exportDir}/${name}`,
+                    })),
+                  ],
+                },
+              },
+            }),
+            new RemoveEmptyScriptsPlugin({}),
+            new MiniCssExtractPlugin({
+              filename: 'css/[name].css',
+            }),
+          ]
+        : []),
+      new ESLintPlugin({
+        extensions: ['ts'],
+      }),
+    ],
     cache: {
       type: 'filesystem',
     },
     optimization: {
-      runtimeChunk: WEBPACK_SERVE ? 'single' : undefined,
+      runtimeChunk: isDev ? 'single' : undefined,
     },
   }
 }
-
-type WebpackConfig = (env: {
-  WEBPACK_WATCH?: true
-  WEBPACK_SERVE?: true
-  WEBPACK_BUILD?: true
-}) => Configuration
-
-export default webpackConfig
