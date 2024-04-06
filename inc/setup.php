1d82b9a6dd760d4f955002a84768e951ffe8bcbb
Radosław Piotrowski <60006721+rplbc@users.noreply.github.com>
2024-04-06 15:07:47 +0200

    refactor: remove unnecessary files (#16)
    * chore: clear acf-json folder
    
    * refactor: remove unnecessary files
    
    * refactor: remove default nav
    
    * refactor: remove anything related to ACF

--- inc/setup.php
+++ inc/setup.php
@@ -6,20 +6,11 @@
  * @package Quick
  */
 
-add_action('init', function () {
-    register_nav_menus([
-        'primary_menu' => __('Header Menu', 'quick'),
-    ]);
-});
-
 add_action('after_setup_theme', function () {
     add_theme_support('title-tag');
 
     add_theme_support('editor-styles');
     add_editor_style('dist/css/editor-styles.css');
-
-    remove_theme_support('core-block-patterns');
-    remove_theme_support('widgets-block-editor');
 });
 
 add_action('wp_enqueue_scripts', function () {
@@ -31,22 +22,4 @@ add_action('wp_enqueue_scripts', function () {
 
     wp_enqueue_script('quick-scripts', get_template_directory_uri() . '/dist/js/main.js', [], $themeVersion, true);
     wp_enqueue_style('quick-styles', get_template_directory_uri() . '/dist/css/styles.css', [], $themeVersion);
-});
-
-add_action('acf/init', function () {
-    if (function_exists('acf_add_options_sub_page')) {
-        $rootPage = acf_add_options_page([
-            'page_title' => wp_get_theme()->get('Name'),
-            'menu_title' => wp_get_theme()->get('Name'),
-            'position' => 1,
-            'post_id' => 'quick_options',
-            'redirect' => false,
-        ]);
-
-        acf_add_options_sub_page([
-            'page_title' => 'Subpage',
-            'menu_title' => 'Subpage',
-            'parent_slug' => $rootPage['menu_slug'],
-        ]);
-    }
 });
