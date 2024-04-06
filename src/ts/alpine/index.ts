18c106e8c30f45d0dc78f289afaf6427a973dd6d
rplbc <radoslawlb@gmail.com>
2024-04-06 13:03:02 +0200

    refactor: remove extra plugins

--- src/ts/alpine/index.ts
+++ src/ts/alpine/index.ts
@@ -1,12 +1,8 @@
 import Alpine from 'alpinejs'
 
-import collapse from '@alpinejs/collapse'
 import focus from '@alpinejs/focus'
-import intersect from '@alpinejs/intersect'
 
-Alpine.plugin(collapse)
 Alpine.plugin(focus)
-Alpine.plugin(intersect)
 
 window.Alpine = Alpine
 
